5159298f059a1fcbf505a3cba7322f7732b33c79
Tomas Trujillo <tomastru114@gmail.com>
2025-02-12 15:41:56 +0100

    Feature: content based algorithm

---
+++ src/shared/algorithms/contentBasedFiltering.ts
@@ -0,0 +1,28 @@
+import { CategoriesDto } from 'src/modules/feed/dto/feed.dto';
+
+function contentBased(
+  likedCategories: CategoriesDto[],
+  likedTags: string[],
+  allPosts: any[],
+) {
+  return allPosts
+    .map((post) => {
+      let score = 0;
+
+      // Assign points based on category matches
+      if (
+        post.categories.some((category) => likedCategories.includes(category))
+      ) {
+        score += 5;
+      }
+
+      // Assign points based on tag matches
+      if (post.tags.some((tag) => likedTags.includes(tag))) {
+        score += 3;
+      }
+
+      return { ...post, score };
+    })
+    .sort((a, b) => b.score - a.score); // Sort posts by score in descending order
+  //.slice(0, 10);  Return only the top 10 posts for pagination
+}
